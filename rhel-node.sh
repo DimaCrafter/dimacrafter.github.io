fe2441b3b8a5c2591df6118ad8917bda4dd29de2
DimaCrafter <DimaCrafter@users.noreply.github.com>
2020-01-17 21:40:09 +0400

    Update rhel-node.sh

--- rhel-node.sh
+++ rhel-node.sh
@@ -10,6 +10,6 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 source ~/.bashrc
 nvm install 12
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
-mgr install yarn
+$mgr install yarn
 unset mgr
 clear
