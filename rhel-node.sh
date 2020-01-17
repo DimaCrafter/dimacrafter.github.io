93e3420bb3bbb3e511c7b668b2de28447272c873
DimaCrafter <DimaCrafter@users.noreply.github.com>
2020-01-17 21:38:50 +0400

    Create rhel-node.sh

---
+++ rhel-node.sh
@@ -0,0 +1,15 @@
+if hash yum 2>/dev/null; then
+  mgr=yum;
+fi
+
+if hash dnf 2>/dev/null; then
+  mgr=dnf;
+fi
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+source ~/.bashrc
+nvm install 12
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
+mgr install yarn
+unset mgr
+clear
